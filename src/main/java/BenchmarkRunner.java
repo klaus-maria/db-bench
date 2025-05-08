4fc20fda2e73141919b5568cc1ca120b17164716
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2025-05-08 23:28:17 +0200

    added system monitoring

--- src/main/java/BenchmarkRunner.java
+++ src/main/java/BenchmarkRunner.java
@@ -1,9 +1,15 @@
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class BenchmarkRunner {
@@ -13,6 +19,7 @@ public class BenchmarkRunner {
     private final int recordCount;
     private final String workload;
     private final String outputPath;
+    private List<SystemSnapshot> snapshots = new ArrayList<>();
 
     public BenchmarkRunner(Database database, int threadCount, int recordCount, String workload, String outputPath) {
         this.database = database;
@@ -27,11 +34,15 @@ public class BenchmarkRunner {
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         // Concurreny-Safe variablen
         AtomicLong opCount = new AtomicLong(0);
+        AtomicBoolean monitor = new AtomicBoolean(true);
         Queue<Long> latencies = new ConcurrentLinkedDeque<>();
+
         double throughput = 0.0;
 
         database.connect();
         database.loadTestData();
+
+        systemMonitoring(monitor);
 
         // jeder Thread stellt einen Nutzer dar, der Anfragen an die Datenbank schickt
         long startTime = System.nanoTime();
@@ -58,6 +69,29 @@ public class BenchmarkRunner {
         executor.awaitTermination(1, TimeUnit.MINUTES);
 
         long runningTime = System.nanoTime() - startTime;
+        monitor.set(false);
         throughput = opCount.get() / (runningTime / 1e9); // division durch 1e9 um auf Sekunden
     }
+
+    private void systemMonitoring(AtomicBoolean running){
+        Thread monitor = new Thread(() -> {
+            OperatingSystemMXBean os = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+            while(running.get()){
+                double cpuLoad = os.getCpuLoad() * 100; // * 100 um auf prozent zu kommen
+                double cpuLoadProcess = os.getProcessCpuLoad() * 100;
+                long usedMemory = os.getTotalMemorySize() - os.getFreeMemorySize();
+                long timestamp = System.currentTimeMillis();
+                snapshots.add(new SystemSnapshot(timestamp, cpuLoad, cpuLoadProcess, usedMemory));
+                try {
+                    Thread.sleep(1000); // system nur jede sekunde checken
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        monitor.start();
+    }
+
+    private record SystemSnapshot(long timestamp, double cpuLoad, double cpuProcessLoad, long usedMemory){};
 }
